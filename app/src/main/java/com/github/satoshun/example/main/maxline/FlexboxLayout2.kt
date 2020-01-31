733784fb41fa7e2c5215fa76d1e31d73d4d23182
SatoShun <shun.sato1@gmail.com>
2020-01-31 00:22:07 +0000

    first

---
+++ app/src/main/java/com/github/satoshun/example/main/maxline/FlexboxLayout2.kt
@@ -0,0 +1,13 @@
+package com.github.satoshun.example.main.maxline
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.flexbox.FlexboxLayout
+
+class FlexboxLayout2 @JvmOverloads constructor(
+  context: Context,
+  attrs: AttributeSet? = null,
+  defStyleAttr: Int = 0
+) : FlexboxLayout(context, attrs, defStyleAttr) {
+
+}
